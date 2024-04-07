12c570ca68b125ffa64629c259937dc88792637a
koichiro59 <rion9092@gmail.com>
2024-04-07 22:14:19 +0900

    loginComplete

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './header';
+import Login from './login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Header/>
+    <Login/>
   </React.StrictMode>
 );
 
---
+++ src/login.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+    return (
+        <div>
+            <h2 style={style.title}>ログイン</h2>
+            <form action="" style={style.form}>
+                <input type="text" style={style.input} placeholder='ユーザー名(必須)'/>
+                <input type="text" style={style.input} placeholder='パスワード(必須)'/>
+                <div>
+                     <button style={style.loginbtn}>ログイン</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const style: {[key: string]: React.CSSProperties} = {
+    title:{
+        textAlign:"center",
+        fontFamily:"sans-serif",
+        fontSize:"24px",
+        fontWeight:"bold",
+        marginTop:"70px"
+    },
+    form:{
+        margin:"-10px auto 0 auto",
+        width:"600px",
+        height:"300px"
+    },
+    input:{
+        display:"block",
+        margin:"25px auto",
+        width:"420px",
+        height:"40px",
+        background:"#eaeaea",
+        border:"solid 1px #fff",
+        borderBottom:"solid 1.5px #444444"
+    },
+    loginbtn:{
+        display:"block",
+        margin:"40px auto",
+        fontFamily:"sans-serif",
+        width:"420px",
+        height:"35px",
+        background:"#ff7c3c",
+        color:"#fff",
+        border:"solid 1px #fff",
+        fontSize:"15px",
+    },
+}
+
+export default Login;
