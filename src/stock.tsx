be0c2e97b6abab9946141916503003f21e59e320
koichiro59 <rion9092@gmail.com>
2024-05-01 09:29:46 +0900

    delete

--- src/stock.tsx
+++ src/stock.tsx
@@ -1,59 +1,262 @@
-import React, {useState,useContext} from "react";
+import React, {useState,useContext,useEffect} from "react";
 import { AuthContext } from "./param"
+import axios from "axios";
+
+type ProductType={
+    product_id:number;
+    product_name:string;
+    price:number;
+    description:string;
+    stock:number;
+  }
 
 const Stock: React.FC = () => {
     const param = useContext(AuthContext);
+    const [select,setSelect]=useState(0);
+    const [count,setCount]=useState<number>(0);
+    const [onContent,setOnContent]=useState(true);
+    const [check,setCheck]=useState(false)
+    const [product,setProduct]=useState<ProductType[]>([]);
+    const [value,setValue]=useState({product_id:0,product_name:"",price:0,description:"",stock:0});
+
+    useEffect(() => {
+        axios.get("http://localhost:3000").then((response) => {
+          const { products } = response.data;
+          setProduct(products);
+        });
+      }, []);
+
+      useEffect(() => {
+        console.log(typeof count)
+      }, [count]);
+
+    const selectValue=(e:any)=>{
+        setValue(product[e.target.value-1])
+    }
+
+    const selectCount=(e:any)=>{
+        const a=parseInt(e.target.value)
+        setCount(a)
+    }   
 
     return(
         <div style={param?.value?style.open:style.close}>
-            <h2 style={style.title}>商品在庫管理</h2>
-            <h3 style={style.disTitle}>在庫処理</h3>
-            <div style={style.disArea}>
-                <input type="text" placeholder="商品名" style={style.disInput}/>
-                <input type="text" placeholder="数量" style={style.disInput}/>
-                <button style={style.disbtn}>商品を入れる</button>
-                <button style={style.disbtn2}>商品を卸す</button>
+            <div style={style.menuArea}>
+                <h3 style={style.title}>商品在庫管理</h3>
+                <ul style={style.tab}>
+                    <li style={style.sell} onClick={()=>setOnContent(true)}>商品を仕入れる</li>
+                    <li style={style.buy} onClick={()=>setOnContent(false)}>商品を卸す</li>
+                </ul>
             </div>
-            <h3 style={style.disTitle}>在庫管理</h3>
-            <table style={style.listTable}>
-                <tr>
-                    <th style={style.listTh}>処理種別</th>
-                    <th style={style.listTh}>処理日時</th>
-                    <th style={style.listTh}>単価</th>
-                    <th style={style.listTh}>数量</th>
-                    <th style={style.listTh}>価格</th>
-                    <th style={style.listTh}>在庫数</th>
-                </tr>
-                <tr>
-                    <td style={style.listTd2n1}>1</td>
-                    <td style={style.listTd2n1}>2023-05-21-09:00:00</td>
-                    <td style={style.listTd2n1}></td>
-                    <td style={style.listTd2n1}>400</td>
-                    <td style={style.listTd2n1}></td>
-                    <td style={style.listTd2n1}></td>
-                </tr>
-            </table>
-        </div>
+            {onContent?
+            <>
+            <div style={style.container}>
+                <div style={style.product2}>
+                    <h3 style={style.subTitle}>商品名</h3>
+                    <select style={style.selectProduct} onChange={(e)=>selectValue(e)}>
+                        {product.map((products)=>(
+                            <option value={products.product_id}>{products.product_name}</option>
+                        ))}
+                    </select>
+                </div>
+                <div style={style.items2}>
+                    <h3 style={style.subTitle}>数量</h3>
+                    <input type="text" style={style.itemsInput} onChange={(e)=>selectCount(e)}/>
+                </div>
+                <div style={style.stock2}>
+                  <h3 style={style.subTitle}>在庫数</h3>
+                  {check?<span style={style.preStock}>0</span>
+                  :<span style={style.preStock}>{value.stock}</span>}
+                  <span style={style.mark}>➡</span>
+                  {check?<span style={style.postStock}>0</span>
+                  :<span style={style.postStock}>{count+value.stock}</span>}
+                </div>
+                <div style={style.total2}>
+                    <h3 style={style.subTitle}>合計金額</h3>
+                    <span style={style.totalCount}>￥{value.price*count}</span>
+                    <button style={style.confirm}>確定</button>
+                </div>
+            </div>
+            </>:
+            <>
+            <div style={style.container}>
+                <div style={style.product}>
+                    <h3 style={style.subTitle}>商品名</h3>
+                    <select style={style.selectProduct} onChange={(e)=>selectValue(e)}>
+                        {product.map((products)=>(
+                            <option value={products.product_id}>{products.product_name}</option>
+                        ))}
+                    </select>
+                </div>
+                <div style={style.items}>
+                    <h3 style={style.subTitle}>数量</h3>
+                    <input type="text" style={style.itemsInput} onChange={(e)=>selectCount(e)}/>
+                </div>
+                <div style={style.stock}>
+                  <h3 style={style.subTitle}>在庫数</h3>
+                  {check?<span style={style.preStock}>0</span>
+                  :<span style={style.preStock}>{value.stock}</span>}
+                  <span style={style.mark}>➡</span>
+                  {check?<span style={style.postStock}>0</span>
+                  :<span style={style.postStock}>{count+value.stock}</span>}
+                </div>
+                <div style={style.total}>
+                    <h3 style={style.subTitle}>合計売上</h3>
+                    <span style={style.totalCount}>￥{value.price*count}</span>
+                    <button style={style.confirm}>確定</button>
+                </div>
+            </div>
+            </>}
+    </div>
     )
 }
 
 const style: {[key: string]: React.CSSProperties} = {
-    close:{
-
-    },
     open:{
         marginLeft:"200px"
     },
+    close:{
+
+    },
+    menuArea:{
+        display:"flex",
+        margin:"30px 0px 0px 40px",
+        background:"#fff",
+        // border:"solid 1px #ff7c3c"
+    },
     title:{
         fontFamily:"Yu Gothic",
-        margin:"30px 0px 10px 30px"
+    },
+    tab:{
+        display:"flex",
+        listStyle:"none",
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    sell:{
+        background:"#ff7c3c",
+        width:"120px",
+        padding:"8px 10px",
+        cursor:"pointer",
+    },
+    buy:{
+        background:"#eaeaea",
+        width:"120px",
+        padding:"8px 10px",
+        marginLeft:"0px",
+        cursor:"pointer",
+    },
+    tabContent:{
+        display:"block"
+    },
+    container:{
+        background:"#ff7c3c",
+        margin:"-16px 0 0 30px",
+        width:"60%",
+    },
+    product:{
+        display:"flex",
+        background:"#eaeaea",
+    },
+    items:{
+        display:"flex",
+        background:"#eaeaea",
+    },
+    stock:{
+        display:"flex",
+        background:"#eaeaea",
+    },
+    total:{
+        display:"flex",
+        background:"#eaeaea",
+    },
+    product2:{
+        display:"flex",
+        background:"#ff7c3c",
+    },
+    items2:{
+        display:"flex",
+        background:"#ff7c3c",
+    },
+    stock2:{
+        display:"flex",
+        background:"#ff7c3c",
+    },
+    total2:{
+        display:"flex",
+        background:"#ff7c3c",
+    },
+    subTitle:{
+        margin:"20px 100px 20px 30px",
+        fontFamily:"Yu Gothic",
+        width:"100px"
+    },
+    selectProduct:{
+        padding:"5px 20px",
+        height:"35px",
+        width:"220px",
+        margin:"auto 20px",
+        textAlign:"left",
+        fontSize:"14px",
+    },
+    itemsInput:{
+        padding:"5px 0px",
+        height:"25px",
+        width:"100px",
+        margin:"auto 40px",
+        textAlign:"center",
+        fontSize:"16px",
+    },
+    preStock:{
+        padding:"5px 0px",
+        height:"25px",
+        width:"100px",
+        margin:"auto 0px auto 20px",
+        background:"#fff",
+        border:"solid 1px #444444",
+        textAlign:"center",
+        fontSize:"16px",
+    },
+    mark:{
+        margin:"auto 10px",
+    },
+    postStock:{
+        padding:"5px 0px",
+        height:"25px",
+        width:"100px",
+        margin:"auto 0px",
+        background:"#fff",
+        border:"solid 1px #444444",
+        textAlign:"center",
+        fontSize:"16px",
+    },
+    totalCount:{
+        padding:"5px 0px",
+        height:"25px",
+        width:"100px",
+        margin:"auto 0px",
+        background:"#fff",
+        border:"solid 1px #444444",
+        textAlign:"center",
+        fontSize:"16px",
+    },
+    confirm:{
+        padding:"5px 20px",
+        height:"35px",
+        width:"100px",
+        margin:"auto 20px auto 300px",
+        textAlign:"center",
+        fontSize:"16px",
+        background:"#fff",
+        border:"solid 1px #444444",
     },
     disTitle:{
         fontFamily:"Yu Gothic",
-        margin:"0px 0px 10px 30px",
+        margin:"30px 0px 0px 40px",
     },
     disArea:{
-        margin:"0px 0px 10px 30px",
+        margin:"0px 0px 30px 30px",
+        display:"flex"
     },
     disInput:{
         display:"block",
@@ -66,23 +269,34 @@ const style: {[key: string]: React.CSSProperties} = {
         borderBottom:"solid 1px #eaeaea",
         borderRadius:"1px"
     },
+    disInput2:{
+        display:"block",
+        width:"240px",
+        height:"35px",
+        background:"#eaeaea",
+        borderTop:"solid 1px #fff",
+        borderRight:"solid 1px #fff",
+        borderLeft:"solid 1px #fff",
+        borderBottom:"solid 1px #eaeaea",
+        borderRadius:"1px"
+    },
     disbtn:{
-        margin:"10px 10px 10px 0px",
         background:"#ff7c3c",
         color:"#fff",
         border:"solid 1px #fff",
         borderRadius:"1px",
         padding:"7px 14px",
-        width:"120px"
+        width:"120px",
+        margin:"0 0 0 20px"
     },
     disbtn2:{
-        margin:"10px 0px 10px -15px",
         background:"#ff7c3c",
         color:"#fff",
         border:"solid 1px #fff",
         borderRadius:"1px",
         padding:"7px 14px",
-        width:"120px"
+        width:"120px",
+        margin:"0 20px 0 0"
     },
     listTable:{
         borderCollapse:"collapse",
@@ -113,7 +327,6 @@ const style: {[key: string]: React.CSSProperties} = {
         fontFamily:"Yu Gothic",
         padding:"15px 20px",
     }
-
 }
 
 export default Stock;
